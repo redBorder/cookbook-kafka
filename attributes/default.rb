3ec905f9d17b709b49bff1fcfe989fde45d6c6f5
cjmateos <mateos83@gmail.com>
2016-08-26 12:01:20 +0200

    Fixed typo in zookeeper port attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,5 @@ default["kafka"]["logfile"] = "/var/log/kafka"
 default["kafka"]["user"] = "kafka"
 default["kafka"]["group"] = "kafka"
 default["kafka"]["port"] = 9092
-default["kafka"]["zk_hosts"] = "localhost:8121"
+default["kafka"]["zk_hosts"] = "localhost:2181"
 default["kafka"]["host_index"] = 0
