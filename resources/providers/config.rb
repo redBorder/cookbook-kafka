960958da36702bd03f27334a0cae9633d691d7d4
Miguel Alvarez <malvarez@redborder.com>
2025-03-07 12:53:27 +0000

    Fix kafka check create topics

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -183,7 +183,7 @@ action :add do
       retries 2
       variables(kafka_topics: kafka_topics, managers_list: managers_list)
       notifies :run, 'bash[create_topics]', :delayed
-      only_if 'nc -zv zookeeper.service 2181'
+      only_if 'nc -zv kafka.service 9092'
     end
 
     service 'kafka' do
