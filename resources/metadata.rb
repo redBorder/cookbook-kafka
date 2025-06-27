265a331b6bb9e12888078aefcb68b6779a4fd8b5
Rafael Gomez <rgomez@redborder.com>
2025-06-27 09:49:29 +0100

    Update version to 2.0.1 and add changelog entry for flush cache removal

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -3,6 +3,6 @@ maintainer       'Eneo Tecnología S.L.'
 maintainer_email 'git@redborder.com'
 license          'AGPL-3.0'
 description      'Installs/Configures kafka'
-version          '2.0.0'
+version          '2.0.1'
 
 depends 'zookeeper'
